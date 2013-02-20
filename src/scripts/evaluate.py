990368d501aeb70fa7810bca2e7031bb577aebbc
Iain Bancarz <ib5@sanger.ac.uk>
2013-02-20 15:02:09 +0000

    Modified to use SampleEvaluator class

--- src/scripts/evaluate.py
+++ src/scripts/evaluate.py
@@ -5,40 +5,20 @@
 Author:  Iain Bancarz, ib5@sanger.ac.uk, January 2013
 """
 
-import os, re
-from calibration import MetricFinder
-from GTC import *
+import json, os, re
+from calibration import SampleEvaluator
 try: 
     import argparse     # optparse is deprecated, using argparse instead
 except ImportError: 
     sys.stderr.write("ERROR: Requires Python 2.7 to run; exiting.\n")
     sys.exit(1)
 
-class evaluator:
-    """Class to evaluate single GTC file on multiple z scores
-
-    Inputs:
-    Path to .json file with z scores and corresponding threshold paths
-    Path to GTC input file
-
-    Output:
-    File with concordance and gain for each z score"""
-
-    def __init__(self, thresholdsPath, gtcPath):
-
-        self.gtc = GTC(gtcPath)
-
-    def evaluate(self):
-        pass
-
-
-
 def main():
     """Method to run as script from command line.  Run with --help for usage."""
     description = "Evaluate concordance/gain for given zCall thresholds on a single GTC file."
     parser = argparse.ArgumentParser(description=description)
     parser.add_argument('--thresholds', required=True, metavar="PATH", 
-                        help="Path to .json file containing thresholds paths")
+                        help="Path to .json file containing threshold .txt paths indexed by z score")
     parser.add_argument('--bpm', required=True, metavar="PATH", 
                         help="BPM .csv manifest file")
     parser.add_argument('--egt', required=True, metavar="PATH", 
@@ -46,7 +26,7 @@ def main():
     parser.add_argument('--gtc', required=True, metavar="PATH", 
                         help="Path to .gtc input file")
     parser.add_argument('--out', required=True, metavar="PATH", 
-                        help="Output file")
+                        help="Path for .json output")
     parser.add_argument('--verbose', action='store_true', default=False,
                         help="Print status information to standard output")
     args = vars(parser.parse_args())
@@ -59,10 +39,9 @@ def main():
     (dirName, fileName) = os.path.split(os.path.abspath(args['out']))
     if fileName=='' or not os.access(dirName, os.R_OK):
         raise OSError("Invalid output path \""+args['out']+"\"")
-    mf = MetricFinder(args['thresholds'], args['bpm'], args['egt'])
-    mf.evaluate(args['gtc'], args['out'], args['verbose'])
-
-
+    
+    eva = SampleEvaluator(args['bpm'], args['egt'])
+    eva.run(args['thresholds'], args['gtc'], args['out'], args['verbose'])
 
 if __name__ == "__main__":
     main()

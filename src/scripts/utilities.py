1c8ba19b55975eb000ea26dc8877234d930c68e8
Iain Bancarz <ib5@sanger.ac.uk>
2013-02-22 15:05:51 +0000

    Renamed and added ThresholdContainer class

--- src/scripts/utilities.py
+++ src/scripts/utilities.py
@@ -5,9 +5,8 @@
 from GTC import *
 from BPM import *
 from EGT import *
-from thresholdContainer import ThresholdContainer
 
-class zCallBase:
+class CallingBase:
 
     """ 'Base' class containing useful methods for zcall subclasses """
 
@@ -69,3 +68,40 @@ class zCallBase:
     def setThresholds(self, thresholds):
         """Set thresholds to given ThresholdContainer object"""
         self.thresholds = thresholds
+
+
+class ThresholdContainer:
+    """Class to contain thresholds, read from thresholds.txt file"""
+
+    def __init__(self, inPath):
+        (self.x, self.y) = self.readThresholds(inPath)
+        
+    def getX(self, i):
+        """Get x threshold for the ith SNP"""
+        return self.x[i]
+
+    def getY(self, i):
+        """Get y threshold for the ith SNP"""
+        return self.y[i]
+
+    def readThresholds(self, inPath):
+        """ Read a thresholds.txt file; return lists of x and y thresholds """
+        thresholdsX = []
+        thresholdsY = []
+        for line in open(inPath, 'r'):
+            line = line.replace("\n", "")
+            if line.find("Tx") != -1:
+                continue
+            else:
+                fields = line.split("\t")
+                if fields[1] != "NA":
+                    tx = float(fields[1])
+                else:
+                    tx = fields[1]
+                if fields[2] != "NA":
+                    ty = float(fields[2])
+                else:
+                    ty = fields[2]
+                thresholdsX.append(tx)
+                thresholdsY.append(ty)
+        return (thresholdsX, thresholdsY)

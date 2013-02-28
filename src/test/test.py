fe5131ac225f3e86daa77a0a68927d72a38e965e
Iain Bancarz <ib5@sanger.ac.uk>
2013-02-28 17:15:07 +0000

    Added getMD5 function and modified test data locations

--- src/test/test.py
+++ src/test/test.py
@@ -4,11 +4,10 @@
 """Unit tests for zcall module
 
 Test cases:
-- prepareThresholds.py creates correct thresholds.txt files (use checksum?)
+- prepareThresholds.py creates correct thresholds.txt files
 - evaluateThresholds.py creates correct evaluation in .json format
 - mergeEvaluation.py correctly merges results and identifies best Z score
-- call.py produces valid Plink binary data
-- sub-tests for BPM, EGT, GTC object creation
+- call.py produces Plink binary data
 
 Required input data:
 - Example BPM, EGT files
@@ -20,27 +19,36 @@ Author:  Iain Bancarz, ib5@sanger.ac.uk
 
 import json, os, sys, unittest
 from hashlib import md5
+from tempfile import mkdtemp
 
 class TestScripts(unittest.TestCase):
 
     """Test command-line python scripts used in WTSI genotyping pipeline"""
 
+    def getMD5(self, inPath):
+        """Get MD5 checksum for contents of given file"""
+        m = md5()
+        m.update(open(inPath).read())
+        checksum = m.digest()
+        return checksum
+
     def setUp(self):
         """Check for input/output directory called 'data' 
 
         Must have appropriate input files"""
-        self.testDir = 'data'
-        self.refDir = self.testDir+'/ref'
-        if not os.access(self.testDir, os.W_OK) \
-                or not os.path.isdir(self.testDir):
+        self.dataDir = 'data'
+        if not os.access(self.dataDir, os.W_OK) \
+                or not os.path.isdir(self.dataDir):
             msg = "Invalid test directory! Should run from: zCall/src\n"
             sys.stderr.write(msg)
             sys.exit(1)
-        self.bpmPath = os.path.join(self.testDir, 'HumanExome-12v1_A.bpm.csv')
-        self.egtPath = os.path.join(self.testDir, 'HumanExome-12v1.egt')
+        self.outDir = mkdtemp(dir=self.dataDir)
+        print "Created output directory", self.outDir
+        self.bpmPath = os.path.join(self.dataDir, 'HumanExome-12v1_A.bpm.csv')
+        self.egtPath = os.path.join(self.dataDir, 'HumanExome-12v1.egt')
         self.gtcPaths = []
         for name in ('gtc00.json', 'gtc01.json'): 
-            self.gtcPaths.append(os.path.join(self.testDir, name))
+            self.gtcPaths.append(os.path.join(self.dataDir, name))
         inPaths = [self.bpmPath, self.egtPath]
         inPaths.extend(self.gtcPaths)
         for inPath in inPaths:
@@ -48,92 +56,77 @@ class TestScripts(unittest.TestCase):
                 sys.stderr.write("Cannot access test input '"+inPath+"'!\n")
                 sys.exit(1)
         self.thresholdJsonName = 'thresholds.json'
-        self.thresholdJson = os.path.join(self.testDir, self.thresholdJsonName)
-        self.thresholdJsonRef = os.path.join(self.refDir,self.thresholdJsonName)
+        self.thresholdJson = os.path.join(self.dataDir, self.thresholdJsonName)
 
     def test_prepareThresholds(self):
         """Prepare thresholds.txt files"""
         zstart = 6
         ztotal = 3
-        if os.path.exists(self.thresholdJson): os.remove(self.thresholdJson)
         outPaths = []
         for i in range(zstart, zstart+ztotal):
             name = 'thresholds_HumanExome-12v1_z0'+str(i)+'.txt'
-            outPath = os.path.join(self.testDir, name)
-            outPaths.append(outPath)
-            if os.path.exists(outPath): os.remove(outPath)
+            outPaths.append(os.path.join(self.outDir, name))
         args = ['zcall/prepareThresholds.py',
                 '--egt', self.egtPath,
-                '--out', self.testDir,
+                '--out', self.outDir,
                 '--config etc/config.ini',
                 '--zstart', str(zstart),
                 '--ztotal', str(ztotal),
                 '--index_name', self.thresholdJsonName]
         self.assertEqual(os.system(' '.join(args)), 0) # run script
-        self.assertTrue(os.access(self.thresholdJson, os.R_OK))
-        index = {}
-        for i in range(ztotal):
-            index[unicode(i+zstart)] = unicode(os.path.abspath(outPaths[i]))
-        self.assertEqual(json.loads(open(self.thresholdJson).read()), index)
-        # md5 checksums for thresholds.txt files
-        # can't use md5 on json output, as order of key/value pairs may vary
-        expected = ['\xaaD\x05\xf0r\xa12\x9a\xc9?\xd9\xfa\xaf\xd8\xb6z', 
-                    "oL^;\x87r'\xa2\xd9%'8/5g\xa7", 
-                    'U\x19\x98:\x92\xb6\x04@A\xe0\x84bE\xd5\r\xf4']
-        for i in range(len(outPaths)):
-            self.assertTrue(os.access(outPaths[i], os.R_OK))
-            m = md5()
-            m.update(open(outPaths[i]).read())
-            checksum = m.digest()
-            self.assertEqual(checksum, expected[i])
+        jsonOut = os.path.join(self.outDir, self.thresholdJsonName)
+        self.assertTrue(os.access(jsonOut, os.R_OK))
+        oldIndex = json.loads(open(self.thresholdJson).read())
+        outPath = os.path.join(self.outDir, self.thresholdJsonName)
+        newIndex = json.loads(open(outPath).read())
+        for z in newIndex.keys():
+            self.assertEqual(self.getMD5(oldIndex[z]), self.getMD5(newIndex[z]))
 
     def test_evaluateThresholds(self):
         """Evaluate thresholds for collections of sample GTC files"""
         argsBase = ('zcall/evaluateThresholds.py',
                     '--egt', self.egtPath,
                     '--bpm', self.bpmPath,
-                    '--thresholds', self.thresholdJsonRef)
+                    '--thresholds', self.thresholdJson)
         for i in range(len(self.gtcPaths)):
             args = list(argsBase)
-            outPath = os.path.join(self.testDir, 'metrics0'+str(i)+'.json')
+            name = 'metrics0'+str(i)+'.json'
+            outPath = os.path.join(self.outDir, name)
             args.extend(['--gtc', self.gtcPaths[i], '--out', outPath ])
             self.assertEqual(os.system(' '.join(args)), 0) # run script
             metricsNew = json.loads(open(outPath).read())
-            oldPath = os.path.join(self.testDir,'ref','metrics0'+str(i)+'.json')
+            oldPath = os.path.join(self.dataDir, name)
             metricsOld = json.loads(open(oldPath).read())
             self.assertEqual(metricsOld, metricsNew)
 
     def test_mergeEvaluation(self):
         """Merge evaluation results and find best Z score"""
-        outPath = os.path.join(self.testDir, 'zEvaluation.json')
+        outPath = os.path.join(self.outDir, 'zEvaluation.json')
         args = ['zcall/mergeEvaluation.py',
                 '--metrics', 'data/metrics.txt',
-                '--thresholds', self.thresholdJsonRef,
+                '--thresholds', self.thresholdJson,
                 '--out', outPath ]
+        print ' '.join(args)
         self.assertEqual(os.system(' '.join(args)), 0) # run script
         resultsNew = json.loads(open(outPath).read())
-        oldPath = os.path.join(self.testDir,'ref','zEvaluation.json')
+        oldPath = os.path.join(self.dataDir, 'zEvaluation.json')
         resultsOld = json.loads(open(oldPath).read())
         self.assertEqual(resultsOld, resultsNew)
 
     def test_call(self):
         """Re-call GTC files using zCall"""
-
         #TODO read output data into PLINK to verify it is well-formatted
-        outPath = os.path.join(self.testDir, 'test.bed')
-        tPath = os.path.join(self.testDir, 'ref', 
-                             'thresholds_HumanExome-12v1_z07.txt')
+        outPath = os.path.join(self.outDir, 'test.bed')
+        tPath = os.path.join(self.dataDir, 'thresholds_HumanExome-12v1_z07.txt')
         args = ['zcall/call.py',
                 '--thresholds', tPath,
                 '--bpm', self.bpmPath,
                 '--egt', self.egtPath,
-                '--gtc', os.path.join(self.testDir, 'gtc.json'),
+                '--gtc', os.path.join(self.dataDir, 'gtc.json'),
                 '--out', outPath,
             ]
         self.assertEqual(os.system(' '.join(args)), 0) # run script
-        m = md5()
-        m.update(open(outPath).read())
-        checksum = m.digest()
+        checksum = self.getMD5(outPath)
         expected = '\x95A\xec\xbd\x87K\x80\xa3\x01\xb2"\x95\xea\x05\xe8\x88'
         self.assertEqual(checksum, expected)
         

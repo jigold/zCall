dbb601cb7052550756b45b1c2f9ac8f222f71e7f
Iain Bancarz <ib5@sanger.ac.uk>
2013-03-21 12:04:48 +0000

    Reading of .json metric list moved from MetricEvaluator into mergeEvaluation.py

--- src/zcall/mergeEvaluation.py
+++ src/zcall/mergeEvaluation.py
@@ -9,25 +9,25 @@ Author:  Iain Bancarz, ib5@sanger.ac.uk, January 2013
 
 import os, sys
 try: 
-    import argparse    
-    from calibration import MetricEvaluator
+    import argparse, json
 except ImportError: 
     sys.stderr.write("ERROR: Requires Python 2.7 to run; exiting.\n")
     sys.exit(1)
+from calibration import MetricEvaluator
 
 def main():
     """Method to run as script from command line.  Run with --help for usage."""
     description = "Evaluate concordance/gain results for multiple thresholds and samples; find the 'best' z score for subsequent use of zCall."
     parser = argparse.ArgumentParser(description=description)
     parser.add_argument('--metrics', required=True, metavar="PATH", 
-                        help="Path to text file containing paths to .json metrics files")
+                        help="Path to .json file containing paths to .json metrics files")
     parser.add_argument('--thresholds', required=True, metavar="PATH", 
                         help="Path to .json file containing threshold .txt paths indexed by z score")
     parser.add_argument('--out', required=False, metavar="PATH", 
                         help="Path for .json output", default=".")
     args = vars(parser.parse_args())
-    
-    MetricEvaluator().writeBest(args['metrics'], args['thresholds'], 
+    metricPaths = json.loads(open(args['metrics']).read())
+    MetricEvaluator().writeBest(metricPaths, args['thresholds'], 
                                 args['out'])
 
 if __name__ == "__main__":

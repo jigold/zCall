5d39c9fe3846572804f69333ae5c6937b0fb890f
Josef Frank <joseffrank@users.noreply.github.com>
2015-02-24 17:28:07 +0100

    Update dropSamplesFromReport_FasterVersion.py
    Some performance enhancements
    (using list comprehension and avoid steadily growing/reallocating lists)

--- additionalScripts/dropSamplesFromReport_FasterVersion.py
+++ additionalScripts/dropSamplesFromReport_FasterVersion.py
@@ -1,40 +1,28 @@
 #! /usr/bin/python
 
-# Thank you to Josh Randall (Sanger) for providing this script!
+# Based on version from Josh Randall
 
 import sys
 
 reportFileName = sys.argv[1]
 dropSamplesFileName = sys.argv[2]
 
-# read sample exclusion file and populate samples set 
-dropSamples = set() 
-for line in open(dropSamplesFileName, 'r'):
-	line = line.strip()
-	dropSamples.add(line)
- 
+# read sample exclusion file and populate samples set
+dropSamples = set([i.strip() for i in open(dropSamplesFileName)])
+
 # open file => returns an iterator
-reportFile = open(reportFileName, 'r')
+reportFile = open(reportFileName)
 
-okColumns = []
+tab = "\t"
 # read first line and fill okColumns with column indices to be kept (the columns not present in the dropSamples)
-headerline = reportFile.next() 
-headers = headerline.strip().split("\t") 
-headerout = headers[:3]
-for i in range(3, len(headers)):
-	id = headers[i].split(".")[0]
-	if id not in dropSamples:
-		okColumns.append(i)
-		headerout.append(headers[i])
+headers = reportFile.next().strip().split(tab) 
+nfields = len(headers)
+okColumns = [0,1,2] + [i for i in range(3, nfields) if headers[i].split(".")[0] not in dropSamples]
 
-# print new header
-print "\t".join(headerout)
- 
-# read the rest of the report file lines, printing only the important columns (okColumns) 
+# rewind file for inclusion of header in uniform filtering
+reportFile.seek(0)
+# read the whole report file, printing only the important columns (okColumns) 
 for line in reportFile:
-	fields = line.strip().split("\t")
-	out = fields[:3]
-	for i in okColumns:
-		out.append(fields[i])
-	print "\t".join(out)
+	fields = line.strip().split(tab)
+	print tab.join([fields[i] for i in okColumns])
 
